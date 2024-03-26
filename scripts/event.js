48e57d59711df9853ac5caa3d62095b0f872c5d2
João Eduardo Santos Pollhuber <jpollhuber@outlook.com>
2024-03-25 17:42:10 -0700

    Add participant list to event.html

--- scripts/event.js
+++ scripts/event.js
@@ -1,30 +1,7 @@
-//Later find a way to put this inside functions
-var joined = false;
-
-async function getUsername() {
-    // Check if the user is logged in:
-    firebase.auth().onAuthStateChanged(user => {
-        if (user) {
-            console.log(user.uid); // Let's know who the logged-in user is by logging their UID
-            currentUser = db.collection("users").doc(user.uid); // Go to the Firestore document of the user
-            currentUser.get().then(userDoc => {
-                // Get the user name
-                let userName = userDoc.data().name;
-                console.log(userName);
-                //$("#name-goes-here").text(userName); // jQuery
-                return username = userName;
-            })
-        }
-        else {
-            console.log("No user is logged in."); // Log a message when no user is logged in
-            return null
-        }
-    })
-}
-
-async function displayEventInfo() {
+function displayEventInfo() {
     let params = new URL(window.location.href);
     let ID = params.searchParams.get("docID");
+    localStorage.setItem("eventID", ID)
 
     db.collection("events")
         .doc(ID)
@@ -34,71 +11,96 @@ async function displayEventInfo() {
             $('#replace-skill').text(doc.data().skill);
             $('#replace-location').text(doc.data().location);
             $('#replace-host').text(doc.data().host);
+            $('#replace-participants').text("aaa");
 
-            return doc;
-        });
+            var participants = doc.data().participants
+            console.log(participants)
+            console.log(doc.data().participants)
+
+            if (participants == null || participants.length == 0) $('#replace-participants').text("None");
+            else {
+                for (let i = 0; i < participants.length; i++) {
+                    $('#replace-participants').text(participants[i] + "\n")
+
+                    if (participants[i] == localStorage.getItem("userName")) {
+                        joined = true;
+                    }
+                }
+            }
+
+        })
 }
 
-//Currently does not work
-/*
-async function checkIfJoined() {
-    username = await getUsername();
-    doc = await displayEventInfo();
-    console.log(username);
-    console.log(doc);
+function updateJoinBtn(joined) {
+    localStorage.setItem("joined", joined)
 
-    var hasJoined = false;
-    if (doc.data().participants.some(e => username)) {
-        console.log("a")
-        hasJoined = true;
-    }
-    else {
-        hasJoined = false;
-    }
-}
-*/
+    let id = ""
+    let buttonContent = ""
 
-function updateJoinBtn() {
-    var id = ""
-    var buttonContent = ""
+    currentEvent = db.collection("events").doc(localStorage.getItem("eventID"))
+    db.collection("events")
+        .doc(localStorage.getItem("eventID"))
+        .get()
+        .then(async doc => {
+            participants = doc.data().participants
+            if (participants == null) participants = [];
 
-    if (joined) {
-        id = "#event-join-button-joined"
-        $("#event-join-button").attr("id", "event-join-button-joined");
-        buttonContent = `
-        <div class="row justify-content-center">
-            <span class="col-auto material-symbols-outlined" style="padding: 0">
-                done_outline
-            </span>
-            <p class="col-auto" style="margin: 0">Event Joined!</p>
-        </div>`
-    }
-    else {
-        id = "#event-join-button"
-        $("#event-join-button-joined").attr("id", "event-join-button");
-        buttonContent = `
-        <div class="row justify-content-center">
-            <span class="col-auto material-symbols-outlined" style="padding: 0">
-                chair
-            </span>
-            <p class="col-auto" style="margin: 0">Join Event</p>
-        </div>`
-    }
+            //Always removes duplicates
+            for (let i = 0; i < participants.length; i++) {
+                if (participants[i] == localStorage.getItem("userName")) {
+                    participants.splice(i, 1);
+                    i = 0;
+                }
+            }
 
-    $(id).empty()
-    $(id).append(buttonContent)
+            if (joined) {
+                id = "#event-join-button-joined"
+                $("#event-join-button").attr("id", "event-join-button-joined");
+                buttonContent = `
+                    <div class="row justify-content-center">
+                        <span class="col-auto material-symbols-outlined" style="padding: 0">
+                            done_outline
+                        </span>
+                        <p class="col-auto" style="margin: 0">Event Joined!</p>
+                    </div>`;
+
+                participants.push(localStorage.getItem("userName"))
+                localStorage.setItem("joined", true)
+            }
+            else {
+                id = "#event-join-button"
+                $("#event-join-button-joined").attr("id", "event-join-button");
+                buttonContent = `
+                    <div class="row justify-content-center">
+                        <span class="col-auto material-symbols-outlined" style="padding: 0">
+                            chair
+                        </span>
+                        <p class="col-auto" style="margin: 0">Join Event</p>
+                    </div>`;
+
+                localStorage.setItem("joined", false)
+            }
+
+            joined = !joined
+            currentEvent.update({
+                participants: participants
+            })
+            $(id).empty()
+            $(id).append(buttonContent)
+
+            displayEventInfo()
+        })
 }
 
 function clickJoinBtn() {
-    //Change this to interact with Database instead of a variable, only changing if
-    //the database is sucessfully updated (might need a loading version)
-    joined = !joined
-    updateJoinBtn()
+    updateJoinBtn(localStorage.getItem("joined") === 'false')
 }
 
-async function setup() {
+function setup() {
     $("#event-join-button").click(clickJoinBtn)
-    updateJoinBtn()
+
+    //Temporary
+    localStorage.setItem("userName", "user")
 
     displayEventInfo()
 }
