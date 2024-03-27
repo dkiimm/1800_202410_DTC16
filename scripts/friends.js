f694127285a5dfd10facf4886c7f54fd6e47b277
Daniel Kim <daniel.kmkyu@gmail.com>
2024-03-27 10:33:20 -0700

    Make friends.html and make template and js for it

---
+++ scripts/friends.js
@@ -0,0 +1,33 @@
+function DisplayCards(userID) {
+  let cardTemplate = document.getElementById('event_template'); // Define cardTemplate
+  let futureEventsContainer = document.getElementById('future-events');
+
+  // Clear previous content
+  futureEventsContainer.innerHTML = "";
+
+  db.collection('users')
+    .doc(userID) // Filter by the specified userID
+    .get()
+    .then(doc => {
+      if (doc.exists) {
+        let card = cardTemplate.content.cloneNode(true);
+
+        // Add event listener to the card button
+        card.querySelector('#event-card').addEventListener("click", () => {
+          // Later add functionality to get data from the specific event
+          window.location = "event.html?docID=" + doc.id;
+        });
+
+        // Set the sport text in the card
+        card.querySelector('#replace-sport').innerText = doc.data().friend;
+
+        // Append the card to the container
+        futureEventsContainer.appendChild(card);
+      } else {
+        console.log("No such document!");
+      }
+    })
+    .catch(error => {
+      console.error('Error getting document: ', error);
+    });
+}
